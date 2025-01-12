000222cc1a3168d806925008fdd9ad8f84dd635c
Color-Kat <katik7979@gmail.com>
2025-01-12 15:10:20 +0300

    user crud

--- nest-microservices/user-service/src/app.service.ts
+++ nest-microservices/user-service/src/app.service.ts
@@ -4,11 +4,10 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class AppService {
     constructor(private configService: ConfigService)  {
-        console.log(configService.get('DATABASE_URL'));
+        // console.log(configService.get('DATABASE_URL'));
     }
 
     getHello(): string {
-        console.log()
         return 'Hi, I\'m Microservice UserService! I use Transport.TCP to response.';
     }
 }
--- nest-microservices/user-service/src/user/user.service.ts
+++ nest-microservices/user-service/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from "../prisma/prisma.service";
@@ -12,6 +12,14 @@ export class UserService {
     }
 
     async create(dto: CreateUserDto): Promise<User> {
+        const existingUser = await this.prisma.user.findUnique({
+            where: { email: dto.email }
+        });
+
+        if (existingUser) {
+            throw new ConflictException(['A user with this email already exists']);
+        }
+
         const user = await this.prisma.user.create({
             data: {
                 username: dto.username,
@@ -40,21 +48,38 @@ export class UserService {
         });
     }
 
-    findOne(id: number) {
-        return this.prisma.user.findUnique({
+    async findOne(id: number): Promise<User> {
+        const user = await this.prisma.user.findUnique({
             where  : { id },
             include: {
                 profile: true,
                 roles  : true
             }
-        })
+        });
+
+        if (!user) {
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+
+        return user;
     }
 
-    update(id: number, updateUserDto: UpdateUserDto) {
-        return `This action updates a #${id} user`;
+    async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+        const user = await this.prisma.user.update({
+            where: {id},
+            data: {
+                ...updateUserDto
+            }
+        });
+
+        return user;
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} user`;
+    async remove(id: number): Promise<User> {
+        const user = await this.prisma.user.delete({
+            where: {id},
+        });
+
+        return user;
     }
 }
